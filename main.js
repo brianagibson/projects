df609e2b5f28bbc2a5f6c8293dd41bd4d32de50f
Brian Gibson <briangibson@Brians-iMac.lan>
2021-08-23 18:51:46 -0400

    support index with main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString)
+const openParam = urlParams.get('open')
+
+const openDate = new Date('August 26, 2021 21:00:00');
+let isOpen = openParam;
+
+const indexRedirect = () => {
+if (isOpen || openDate < Date.now()) { 
+    'https://brianagibson.github.io/projects/surprise/index.html?open=true'; 
+    } else {
+    window.location = 'https://brianagibson.github.io/projects/surprise/index.html';
+    };
+};
+
+window.onload = indexRedirect;
